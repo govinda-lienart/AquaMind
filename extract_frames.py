90fe0e1c5b590b755573c85c0ecbd17f3b076c31
Govinda Lienart <69435075+govinda-lienart@users.noreply.github.com>
2026-05-06 10:00:48 +0700

    update extract_frames_format_subfolder

--- extract_frames.py
+++ extract_frames.py
@@ -1,12 +1,44 @@
+#IMPORTS
 import cv2
 import os
+import mysql.connector 
+from datetime import datetime 
 
-cap = cv2.VideoCapture("videos/IMG_9856.MOV") # loading the video
+# CONNECT WITH SQL
+conn = mysql.connector.connect( # the pipe - "phone line"
+    host='localhost',
+    port=3306,
+    user='root',
+    password='aquamind',
+    database='aquamind'
+)
+# CREATE AN SQL EXECUTOR
+cursor = conn.cursor() # executor - queries - tele operator - action part of the connect pipe
+
+# FORMATING VIDEO AND TIME NAMING
+# time_formating
+format_now = datetime.now().strftime("%Y%m%d_%H%M")
+
+# video_format
+video_path = "videos/IMG_9856.MOV"
+video_name_ext = os.path.basename(video_path) 
+video_name = os.path.splitext(video_name_ext)[0]
+
+# time and video format
+video_folder_name = f"frames_{video_name}_{format_now}"
+
+# LOADING THE VIDEO
+cap = cv2.VideoCapture(video_path) # loading the video
+
+# CREATE FRAME DIRECTORY
 os.makedirs("frames", exist_ok=True) # creat dirctory frame in case it doesnt exist
+os.makedirs(f"frames/{video_folder_name}", exist_ok=True) # creat dirctory frame in case it doesnt exist
+
+
+# EXTRACT FRAME PER SEC
 fps = round(cap.get(cv2.CAP_PROP_FPS)) # fps prints 59.92 - not round - issue with modulo - therefore round.
-print (fps) 
-
-
+print(f"Number of frames per second: {fps}")              
+# LOOP TO EXTRACT FRAME AND STORE DATA IN SQL DB
 frame_count = 0
 
 while True:
@@ -17,12 +49,21 @@ while True:
 
     if frame_count % fps == 0:
         print (f"saving frame {frame_count}")
-        filename = f"frames/frame_{frame_count}.jpg"
+        filename = f"frames/{video_folder_name}/frame_{frame_count}.png"
         cv2.imwrite(filename,frame)
-
+        timestamp = frame_count / fps       
+        cursor.execute(
+            "INSERT INTO frames (video_path, frame_path, frame_number, timestamp, extracted_at) VALUES (%s, %s, %s, %s, %s)",
+            (video_path, filename, frame_count, timestamp, datetime.now())
+        )
     frame_count += 1
 
+# SAVE SQL DB
+conn.commit() # saves all the inserts to the database permanently
+
+# CLOSURE OF THE PIPE
 cap.release() # close video reading
+conn.close()  # closes the connection to MySQL
 print("Done")
 
 
