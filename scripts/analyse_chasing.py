9b602edc575e105cb86501e08674dbca7f59d764
Govinda Lienart <69435075+govinda-lienart@users.noreply.github.com>
2026-08-12 18:57:32 +0700

    stage7: scaffold analyse_chasing.py structure

--- scripts/analyse_chasing.py
+++ scripts/analyse_chasing.py
@@ -1,6 +1,8 @@
 
+#---------------
 # IMPORTS
-from logging import basicConfig
+#---------------
+
 import os
 import yaml
 import pandas as pd
@@ -15,9 +17,24 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger=logging.getLogger(__name__)
 
+#---------------
 # CONFIGS
-CONFIG_PATH = 'config.yaml' 
-with open(CONFIG_PATH) as f:
+#---------------
+
+# loading tank related data
+with open('config.yaml') as f:
     cfg = yaml.safe_load(f)['analyse_behaviour']
+config_2349 = grab_video_name('IMG_2349_new')
 logger.info(f'configuration data: {cfg}')
 
+#---------------
+# HELPER FUNCTIONS 
+#---------------
+
+
+
+#---------------
+# ENTRY POINT
+#---------------
+
+if __name__ == "__main__"
