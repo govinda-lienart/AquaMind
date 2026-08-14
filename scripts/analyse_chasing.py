3f6c2fc2259a7adba5a7ea4d86b42ec2599f16c1
Govinda Lienart <69435075+govinda-lienart@users.noreply.github.com>
2026-08-14 15:05:43 +0700

    stage7: behavioural scripts update

--- scripts/analyse_chasing.py
+++ scripts/analyse_chasing.py
@@ -1,7 +1,4 @@
-"""usage"""
-
-
-
+"""usage e.g python -m scripts.analyse_chasing --video_name IMG_2349"""
 
 #---------------
 # IMPORTS
@@ -40,20 +37,46 @@ def grab_video_name(video_name):
     tank_width_px = video_cfg['tank_width_px']
     tank_width_cm = video_cfg['tank_width_cm']
     calibration_secs = video_cfg['calibration_secs']
+    surface_y_px = video_cfg['surface_y_px']
+    bottom_y_px = video_cfg['bottom_y_px']
     pixels_per_cm = tank_width_px / tank_width_cm
     banner('LOADING CONFIGURATION')
-    logger.info(f'loaded cfg: video_cfg = {video_cfg}, tank_width_px = {tank_width_px}, tank_width_cm = {tank_width_cm},  pixels_per_cm = {pixels_per_cm}, calibration_secs = {calibration_secs}')
-    return parquet_path, pixels_per_cm, calibration_secs
+    logger.info(f'loaded cfg: video_cfg = {video_cfg}, tank_width_px = {tank_width_px}, tank_width_cm = {tank_width_cm},  pixels_per_cm = {pixels_per_cm}, calibration_secs = {calibration_secs}, surface_y_px = {surface_y_px}, bottom_y_px = {bottom_y_px}')
+    return parquet_path, pixels_per_cm, calibration_secs, surface_y_px, bottom_y_px
 
 #---------------
 # MAIN FUNCTION
 #---------------
 
 
-def main (parquet_path, pixels_per_cm, calibration_secs):
+def main(parquet_path, pixels_per_cm, calibration_secs, surface_y_px, bottom_y_px):
+    
+    # reading parquet file and reorganizing df
     df = pd.read_parquet(parquet_path)
-    banner_sub ('LOADING PARQUET FILE')
-    logger.info (f'quick view on parquet file': {df.to_string()})
+    banner_sub('LOADING PARQUET FILE')
+    logger.info(f'{df.head().to_string()}')
+    banner_sub('EXAMPLE FILTER SELECT ABOVE CALIBRATION')
+    print((df['timestamp'] >= calibration_secs).head())
+    banner_sub('APPLYING FILTER WITH MASK')
+    df = df[df['timestamp'] >= calibration_secs]
+    logger.info(f'{df.head().to_string()}')
+    banner_sub('DATAFRAME - AFTER CALIBRATION TRIM')
+    logger.info(df.shape)
+    banner_sub('SELF-MERGE ON FRAME_NUMBER — combine every fish with every other fish, per frame')
+    pairs = df.merge(df, on='frame_number', suffixes=('_a', '_b')) #  every row with frame_number == 600 from the left copy gets paired with every row with frame_number == 600 from the right copy, one at a time.
+    logger.info(pairs[['frame_number', 'fish_id_a', 'fish_id_b']].head(16).to_string())
+    banner_sub('PAIRS - SHAPE')
+    logger.info(pairs.shape)
+    banner_sub('PAIRS - COLUMNS')
+    logger.info(pairs.columns) 
+    pairs = pairs[pairs['fish_id_a'] < pairs['fish_id_b']] #  Row (1,1) → 1 < 1 → False # Row (1,2) → 1 < 2 → True #Row (2,3) → 2 < 3 → True
+    banner_sub('PAIRS - SHAPE AFTER FILTER')
+    logger.info(pairs.shape)
+    banner_sub('DISTANCE BETWEEN PAIRS')
+    pairs['distance_cm'] = np.hypot (pairs[])
+
+
+
 
 
 #---------------
@@ -63,9 +86,10 @@ def main (parquet_path, pixels_per_cm, calibration_secs):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="imports video configs")
     parser.add_argument("--video_name",
+                        default="IMG_2349",
                         help="indicate which video you want to analyse")
-    args = parser.parse_arg()
+    args = parser.parse_args()
     parquet_path, pixels_per_cm, calibration_secs, surface_y_px, bottom_y_px = grab_video_name(args.video_name)
-    main(parquet_path, pixels_per_cm, calibration_secs)
+    main(parquet_path, pixels_per_cm, calibration_secs, surface_y_px, bottom_y_px)
 
  
